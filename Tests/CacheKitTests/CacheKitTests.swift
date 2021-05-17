3d4e79c82883d983be2e0fcbe31398eadc234da1
BJ Beecher <beecherbj@gmail.com>
2021-05-17 11:23:46 -0400

    added tests

--- Tests/CacheKitTests/CacheKitTests.swift
+++ Tests/CacheKitTests/CacheKitTests.swift
@@ -1,4 +1,37 @@
 import XCTest
 @testable import CacheKit
 
-final class CacheKitTests: XCTestCase {}
+final class CacheKitTests: XCTestCase {
+    
+    var store = [WrappedKey<UUID> : Entry<String>]()
+    
+    lazy var cache = Cache<UUID, String> { entry, wrappedKey in
+        self.store[wrappedKey] = entry
+    } getValue: { wrappedKey in
+        self.store[wrappedKey]
+    } removeValue: { wrappedKey in
+        self.store.removeValue(forKey: wrappedKey)
+    }
+
+    
+    let key = UUID()
+    let value = "Tommy"
+    
+    func testInsert(){
+        cache.insert(value, forKey: key)
+        
+        assert(store[.init(key)]?.value == value)
+    }
+    
+    func testGet(){
+        cache.insert(value, forKey: key)
+        let returned = cache.value(forKey: key)
+        assert(returned == value)
+    }
+    
+    func testDelete(){
+        cache.removeValue(forKey: key)
+        
+        assert(store[.init(key)] == nil)
+    }
+}
